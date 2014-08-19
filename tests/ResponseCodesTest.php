60ff74044690688c023c374843715ea351d22a12
Remco Tolsma <info@remcotolsma.nl>
2014-08-19 15:01:42 +0200

    Fixed tests to the new namespace.

--- tests/ResponseCodesTest.php
+++ tests/ResponseCodesTest.php
@@ -13,19 +13,19 @@ class Pronamic_WP_Pay_Gateways_OmniKassa_ResponseCodesTest extends PHPUnit_Frame
 	 * @dataProvider status_matrix_provider
 	 */
 	public function test_transform( $responseCode, $expected ) {
-		$status = Pronamic_WP_Pay_OmniKassa_ResponseCodes::transform( $responseCode );
+		$status = Pronamic_WP_Pay_Gateways_OmniKassa_ResponseCodes::transform( $responseCode );
 
 		$this->assertEquals( $expected, $status );
 	}
 
 	public function status_matrix_provider() {
 		return array(
-			array( Pronamic_WP_Pay_OmniKassa_ResponseCodes::TRANSACTION_SUCCES, Pronamic_WP_Pay_Statuses::SUCCESS ),
-			array( Pronamic_WP_Pay_OmniKassa_ResponseCodes::AUTHORIZATION_LIMIT, Pronamic_WP_Pay_Statuses::FAILURE ),
-			array( Pronamic_WP_Pay_OmniKassa_ResponseCodes::AUTHORIZATION_REFUSED, Pronamic_WP_Pay_Statuses::FAILURE ),
-			array( Pronamic_WP_Pay_OmniKassa_ResponseCodes::CANCELLATION_OF_PAYMENT, Pronamic_WP_Pay_Statuses::CANCELLED ),
-			array( Pronamic_WP_Pay_OmniKassa_ResponseCodes::PENDING_TRANSACTION, Pronamic_WP_Pay_Statuses::OPEN ),
-			array( Pronamic_WP_Pay_OmniKassa_ResponseCodes::REQUEST_TIMEOUT, Pronamic_WP_Pay_Statuses::EXPIRED ),
+			array( Pronamic_WP_Pay_Gateways_OmniKassa_ResponseCodes::TRANSACTION_SUCCES, Pronamic_WP_Pay_Statuses::SUCCESS ),
+			array( Pronamic_WP_Pay_Gateways_OmniKassa_ResponseCodes::AUTHORIZATION_LIMIT, Pronamic_WP_Pay_Statuses::FAILURE ),
+			array( Pronamic_WP_Pay_Gateways_OmniKassa_ResponseCodes::AUTHORIZATION_REFUSED, Pronamic_WP_Pay_Statuses::FAILURE ),
+			array( Pronamic_WP_Pay_Gateways_OmniKassa_ResponseCodes::CANCELLATION_OF_PAYMENT, Pronamic_WP_Pay_Statuses::CANCELLED ),
+			array( Pronamic_WP_Pay_Gateways_OmniKassa_ResponseCodes::PENDING_TRANSACTION, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_OmniKassa_ResponseCodes::REQUEST_TIMEOUT, Pronamic_WP_Pay_Statuses::EXPIRED ),
 			array( 'not existing response code', null ),
 		);
 	}
