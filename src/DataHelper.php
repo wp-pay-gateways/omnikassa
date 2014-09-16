9b304a431d8cd32a1e03582521da1994bd044a5b
Remco Tolsma <info@remcotolsma.nl>
2014-09-16 15:04:41 +0200

    Added an OmniKassa data helper class to sanitize data.

---
+++ src/DataHelper.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Title: OmniKassa data helper class
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_OmniKassa_DataHelper {
+	/**
+	 * N - Indicates that a numerical value [0-9] is accepted
+	 *
+	 * @var array
+	 */
+	private static $characters_n = array( '0-9' );
+
+	/**
+	 * A - Indicates that an alphabetical value [aA-zZ] is accepted
+	 *
+	 * @var array
+	 */
+	private static $characters_a = array( 'A-Z', 'a-z' );
+
+	/**
+	 * A + N
+	 *
+	 * @var array
+	 */
+	private static $characters_an = array( 'A-Z', 'a-z', '0-9' );
+
+	//////////////////////////////////////////////////
+
+	/**
+ 	 * Alphanumerical, free text
+ 	 *
+ 	 * @param string $string
+ 	 * @param string $max
+ 	 * @return string
+ 	 */
+	public static function an( $string, $max = null ) {
+		return Pronamic_WP_Pay_DataHelper::filter( self::$characters_an, $string, $max );
+	}
+}
