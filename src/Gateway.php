76752838dbb93edee990112bf5bbd91d36efd266
I <info@remcotolsma.nl>
2016-10-11 15:32:49 +0200

    Use the new $payment->format_string() function.

--- src/Gateway.php
+++ src/Gateway.php
@@ -83,7 +83,7 @@ class Pronamic_WP_Pay_Gateways_OmniKassa_Gateway extends Pronamic_WP_Pay_Gateway
 
 		$this->client->set_customer_language( Pronamic_WP_Pay_Gateways_OmniKassa_LocaleHelper::transform( $payment->get_language() ) );
 		$this->client->set_currency_numeric_code( $payment->get_currency_numeric_code() );
-		$this->client->set_order_id( Pronamic_WP_Pay_Gateways_OmniKassa_Util::get_order_id( $this->config->order_id, $payment ) );
+		$this->client->set_order_id( $payment->format_string( $this->config->order_id ) );
 		$this->client->set_normal_return_url( home_url( '/' ) );
 		$this->client->set_automatic_response_url( home_url( '/' ) );
 		$this->client->set_amount( $payment->get_amount() );
